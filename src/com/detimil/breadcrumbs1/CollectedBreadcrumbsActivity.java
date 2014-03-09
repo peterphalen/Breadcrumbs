1964013baeb407a21b96bc28e233c6ebd636ea75
Peter Phalen <peter.phalen@gmail.com>
2014-03-09 12:50:39 -0400

    Fixed database without contentprovider or requery take that google

--- src/com/detimil/breadcrumbs1/CollectedBreadcrumbsActivity.java
+++ src/com/detimil/breadcrumbs1/CollectedBreadcrumbsActivity.java
@@ -1,13 +1,12 @@
 package com.detimil.breadcrumbs1;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.ListActivity;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -18,26 +17,22 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-import android.app.LoaderManager.LoaderCallbacks;
 
 
 	
 
 @SuppressLint("NewApi")
-public class CollectedBreadcrumbsActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor>  {
+public class CollectedBreadcrumbsActivity extends Activity  {
 
 	ListView listview;
-	private DatabaseHandler db;
+	DatabaseHandler db;
 	SimpleCursorAdapter adapter; 
-	CursorLoader cursorLoader;
-	LoaderManager loadermanager;
+	Cursor cursor;
 	
 	private static final String TAG="CursorLoader";
 
 	
-	
-	private static final int LOADER_ID = 1;    
-    
+	    
     @SuppressLint("NewApi")
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,22 +40,27 @@ public class CollectedBreadcrumbsActivity extends ListActivity implements Loader
         setContentView(R.layout.activity_collected_breadcrumbs);
         listview = (ListView) findViewById(android.R.id.list);
         
-       
+		
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+
+    	db = new DatabaseHandler(this);
+
+        cursor = db.getAllLabels();
 
         adapter = new SimpleCursorAdapter(this,
         		android.R.layout.simple_list_item_1,
-        		null, 
+        		cursor, 
         		new String[] { "label" },
         		new int[] { android.R.id.text1 }, 
         		0);
         listview.setAdapter(adapter);
         Log.v(TAG,"ListAdapter is set");
-        loadermanager = getLoaderManager();
-        loadermanager.initLoader(LOADER_ID, null, this);
         
-        Log.v(TAG,"We've made it past initLoader");
-
-            
+    	
     this.listview.setOnItemClickListener(new OnItemClickListener() {
 
     public void onItemClick(AdapterView<?> l, View v, int position, long id) {
@@ -123,9 +123,11 @@ public class CollectedBreadcrumbsActivity extends ListActivity implements Loader
 
                 	Breadcrumb breadcrumb = db.getBreadcrumb(_id);
                     db.deleteBreadcrumb(breadcrumb);
+                    cursor = db.getAllLabels();
+                    adapter.changeCursor(cursor);
                     }
                 
-            }
+;            }
         });
 
 
@@ -137,39 +139,5 @@ public class CollectedBreadcrumbsActivity extends ListActivity implements Loader
     });
 
 }
-    
-
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
-     String[] PROJECTION = new String[] { DatabaseHandler._id + DatabaseHandler.KEY_LABEL };
-     Uri CONTENT_URI = CrumbContentProvider.CONTENT_URI;
-     cursorLoader = new CursorLoader(this, CONTENT_URI, PROJECTION, null, null, null);
-     Log.v(TAG, "onCreateLoader: We finished onCreateLoader");
-
-     return cursorLoader;
-    }
-    
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        Log.v(TAG, "onLoadFinished: We're on onLoadFinished");
-
-          cursor.moveToFirst();
-          String labels = cursor.getString(3);
-          while(cursor.moveToNext());
-          Log.v(TAG, "onLoadFinished: We're done with onLoadFinished");
-
-      }
-    
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-    	 if (adapter != null){
-             adapter.swapCursor(null);}
-             else{
-           	  Log.v(TAG,"OnLoadFinished: mAdapter is null");}
-    }
-
 
 }
