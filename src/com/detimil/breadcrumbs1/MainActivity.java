4dccf55154ece845d70839f455ad8094a516f016
Peter Phalen <peter.phalen@gmail.com>
2014-03-24 09:56:24 -0400

    trying googles code for locationservices

--- src/com/detimil/breadcrumbs1/MainActivity.java
+++ src/com/detimil/breadcrumbs1/MainActivity.java
@@ -1,26 +1,40 @@
 package com.detimil.breadcrumbs1;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Criteria;
+import android.content.IntentSender;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.analytics.tracking.android.EasyTracker;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 
-public class MainActivity extends Activity {
-	protected LocationManager locationManager;
-	protected LocationManager locationManagerRev;
+public class MainActivity extends FragmentActivity implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener {
+	
+	
+	
+    // Global variable to hold the current location
+    Location mCurrentLocation;
+	
+	private LocationClient mLocationClient;
 	protected WifiManager wifi;
+	LocationManager locationManager;
 	MyCurrentLocationListener locationListener;
 	private String bestProvider;
 	private Location lastKnownLocation;
@@ -28,7 +42,149 @@ public class MainActivity extends Activity {
 	private double BREADCRUMB_LATITUDE;
 	private double BREADCRUMB_LONGITUDE;
 	
+	private final static int
+    CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	
 	private static final String TAG = "MyActivity";
+	
+	// Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+    /*
+     * Handle results returned to the FragmentActivity
+     * by Google Play services
+     */
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        // Decide what to do based on the original request code
+        switch (requestCode) {
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+            /*
+             * If the result code is Activity.RESULT_OK, try
+             * to connect again
+             */
+                switch (resultCode) {
+                    case Activity.RESULT_OK :
+                    /*
+                     * Try the request again
+                     */
+                    break;
+                }
+        }
+     }
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.
+                        isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates",
+                    "Google Play services is available.");
+            // Continue
+            return true;
+        // Google Play services was not available for some reason
+        } else {
+            // Get the error code
+            int errorCode = resultCode;
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    errorCode,
+                    this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment =
+                        new ErrorDialogFragment();
+                // Set the dialog in the DialogFragment
+                errorFragment.setDialog(errorDialog);
+                // Show the error dialog in the DialogFragment
+                errorFragment.show(getSupportFragmentManager(),
+                        "Location Updates");
+                return false;
+
+            }
+        }
+		return true;
+    }
+    
+    /*
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // Display the connection status
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+
+    }
+    /*
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
+    }
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @SuppressWarnings("deprecation")
+	@Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            showDialog(connectionResult.getErrorCode());
+        }
+    }
+	
 	    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,33 +197,29 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
 				
-		// Get the location manager	
-				locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		/*
+         * Create a new location client, using the enclosing class to
+         * handle callbacks.
+         */
+		int resp = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
 
-		// Determine the device's best location provider, without worrying about
-		// costs like battery life or response time.
-		
-		// TODO Decide what criteria are most important to this app. Probably response time..
-		
-		// TODO If there is no provider enabled we have to prompt the user to enable one.
+		  if(resp == ConnectionResult.SUCCESS){
+		        mLocationClient = new LocationClient(this, this, this);
+		  }
+		  else{
+		   Toast.makeText(this, "Google Play Service Error " + resp, Toast.LENGTH_LONG).show();}
+	}
+        
 				
-				Criteria criteria = new Criteria();
-				bestProvider = locationManager.getBestProvider(criteria, false);
-				
-
-
-				// Get the last known location from the provider
-				if (locationManager != null){	
-				lastKnownLocation = locationManager.getLastKnownLocation(bestProvider);}
-
-			}
 	
 	  @Override
 	  public void onStart() {
 	    super.onStart();
 	    EasyTracker.getInstance(this).activityStart(this);  // Google analytics.
+	    
+	 // Connect the client.
+        mLocationClient.connect();
 	  }
 	
 	protected void onResume(){
@@ -76,6 +228,9 @@ public class MainActivity extends Activity {
 				
 		//Register locationListener to locationManager and start getting updates as to your current location
 
+		lastKnownLocation = mLocationClient.getLastLocation();
+
+		
 		locationListener = new MyCurrentLocationListener();
 		locationManager.requestLocationUpdates(bestProvider, 0, 0, locationListener);
 
@@ -205,9 +360,10 @@ public class MainActivity extends Activity {
 	
 	protected void onStop(){
 		super.onStop();
-
-		locationManager.removeUpdates(locationListener);
-		if(locationManager==null){Log.d(TAG, "While stopping your locationManager is NULL");}else{Log.d(TAG, "Your locationManager is NOT NULL when your app stops");};
+		// Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        locationManager.removeUpdates(locationListener);
+		if(locationManager==null){Log.d(TAG, "While pausing your locationManager is NULL");}else{Log.d(TAG, "Your locationManager is NOT NULL when your app pauses");};
 	    EasyTracker.getInstance(this).activityStop(this);  // Google analytics.
 
 	}
